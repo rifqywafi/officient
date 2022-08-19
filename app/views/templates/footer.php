60f7abbc0228b70565716d9025a98b3fde42e002
rifqywafi <rifqywafianerdza@gmail.com>
2022-08-19 15:25:02 +0700

    2nd commit

--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -1,5 +1,8 @@
+</div>
+</div>
 <script src="<?= BASEURL;?>/js/bootstrap.js"></script>
 <script src="<?= BASEURL;?>/js/bootstrap.bundle.js"></script>
 <script src="<?= BASEURL;?>/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="<?= BASEURL;?>/js/sidebar.js"></script>
 </body>
 </html>
--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -5,7 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?= BASEURL;?>/css/bootstrap.css">
+    <link rel="stylesheet" href="<?= BASEURL;?>/icons/bootstrap-icons.css">
     <link rel="stylesheet" href="<?= BASEURL;?>/css/style.css">
+    <script type="text/javascript" src="<?= BASEURL;?>/js/jquery.js"></script>
     <title>OFC | <?= $data['judul'] ?></title>
 </head>
 <body>
@@ -13,25 +15,29 @@
     <div class="row flex-nowrap">
         <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-light border">
             <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-dark min-vh-100">
-                <a href="/" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+                <a href="<?= BASEURL; ?>" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-dark">
                     <span class=" d-sm-inline"><img src="<?= BASEURL;?>/img/officient.png" style="width:200px;height:75px;;"></span>
                 </a>
-                <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
+                <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start">
+                <div id="menu">
                     <li>
-                        <a href="#" class="nav-link px-0 align-middle">
-                        <i class="fs-4 bi-table"></i> <span class="ms-1 d-none d-sm-inline">Home</span></a>
+                        <a href="<?= BASEURL;?>" class="nav-link px-0 align-middle">
+                        <i class="fs-4 bi-house-fill"></i> <span class="ms-1 d-none d-sm-inline">Home</span></a>
                     </li>
                     <li>
-                        <a href="" class="nav-link px-1 align-middle collapsed" data-bs-toggle="collapse" data-bs-target="#dashboard-collapse" aria-expanded="false">
-                        Dashboard
-                        </a>
-                        <div class="collapse" id="dashboard-collapse">
-                        <ul class="nav-link nav list-unstyled fw-normal pb-1 bg-light small">
-                            <li><a href="#" class="nav-link d-inline-flex text-decoration-none rounded">Overview</a></li>
-                            <li><a href="#" class="nav-link d-inline-flex text-decoration-none rounded">Weekly</a></li>
-                            <li><a href="#" class="nav-link d-inline-flex text-decoration-none rounded">Monthly</a></li>
-                            <li><a href="#" class="nav-link d-inline-flex text-decoration-none rounded">Annually</a></li>
-                        </ul>
+                        <a href="#karyawan-collapse" id="karyawan-toggle" class="nav-link px-0 align-middle" data-bs-toggle="collapse" aria-controls="karyawan-collapse" aria-expanded="false">
+                        <i class="fs-4 bi-people mr-1"></i> <span class="ms-1 d-none d-sm-inline">Karyawan</span><i class="bi bi-caret-down-fill mx-1 py-1" style="font-size:12px;"></i></a>
+                        <div class="collapse navbar-collapse" id="karyawan-collapse">
+                            <ul class="nav-link nav fw-normal pb-1 bg-light small">
+                                <li class="nav-item"><a href="<?= BASEURL;?>/data" class="nav-link">
+                                <i class="fs-4 bi-folder-fill mx-2"></i>Data</a></li>
+                                <li class="nav-item"><a href="<?= BASEURL;?>/absensi" class="nav-link">
+                                <i class="fs-4 bi-clipboard-check-fill mx-2"></i>Absensi</a></li>
+                                <li class="nav-item"><a href="<?= BASEURL;?>/izin" class="nav-link">
+                                <i class="fs-4 bi-book-fill mx-2"></i>Laporan</a></li>
+                                <li class="nav-item"><a href="<?= BASEURL;?>/izin" class="nav-link">
+                                <i class="fs-4 bi-envelope-fill mx-2"></i>Izin</a></li>
+                            </ul>
                         </div>
                     </li>
                     <li>
@@ -40,16 +46,18 @@
                     </li>
                     <li>
                         <a href="#" class="nav-link px-0 align-middle">
-                            <i class="fs-4 bi-table"></i> <span class="ms-1 d-none d-sm-inline">About</span></a>
+                            <i class="fs-4 bi-info-circle"></i> <span class="ms-1 d-none d-sm-inline">About</span></a>
                     </li>
                 </ul>
                 <hr>
+                </div>
                 <div class="dropdown pb-4">
-                    <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-                        <img src="" alt="" width="30" height="30" class="rounded-circle">
-                        <span class="d-none d-sm-inline mx-1"></span>
+                    <a href="#menuuser" class="d-flex align-items-center text-dark dropdown-toggle" id="dropdownUser" data-bs-toggle="dropdown" aria-controls="menuuser" aria-expanded="false">
+                        <i class="bi bi-person-circle-fill" alt="profile" width="30" height="30" class="rounded-circle">
+                        </i>
+                        <span class="d-none d-sm-inline mx-1 text-dark">User</span>
                     </a>
-                    <ul class="dropdown-menu dropdown-menu-light text-small shadow">
+                    <ul class="dropdown-menu dropdown-menu-dark text-small-dark shadow" id="menuuser">
                         <li><a class="dropdown-item" href="#">New project...</a></li>
                         <li><a class="dropdown-item" href="#">Settings</a></li>
                         <li><a class="dropdown-item" href="#">Profile</a></li>
@@ -60,6 +68,5 @@
                     </ul>
                 </div>
             </div>
-        </div>
-        <div class="col py-3">
-            
+<div class="col py-3">
+
