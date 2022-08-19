e54482d5617108ac1e4cac0f74b3f81eb5cee8dc
apran17 <aprananugrah2005@gmail.com>
2022-08-19 15:28:28 +0700

    Merge branch 'rifqy' of https://github.com/rifqywafi/officient into Apran

--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -1,5 +1,7 @@
-<script src="<?= BASEURL;?>/js/bootstrap.js"></script>
-<script src="<?= BASEURL;?>/js/bootstrap.bundle.js"></script>
-<script src="<?= BASEURL;?>/js/bootstrap.min.js"></script>
+<script src="<?= BASEURL; ?>/css/bootstrap.js"></script>
+<script src="<?= BASEURL; ?>/css/bootstrap.bundle.js"></script>
+<script src="<?= BASEURL; ?>/css/dataTable.min.js"></script>
+<script src="<?= BASEURL; ?>/css/jquery.js"></script>
 </body>
+
 </html>
